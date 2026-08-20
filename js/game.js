4be1052e1ca28c28b5cebb940ae8dce28f7b3b95
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-20 09:58:01 +0200

    new js files added

--- js/game.js
+++ js/game.js
@@ -1,10 +1,12 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+
+
+let world = new World();
 
 function init(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    console.log("my character ist,", character);
+    console.log("my character is,", world.character);
     
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -6,4 +6,8 @@ class MovableObject {
     moveRight() {
         console.log("Move right");
     }
+
+    moveLeft(){
+        
+    }
 }
