ebd8c2c322a434038ee2272962930aa14129b0ec
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 18:42:53 +0200

    clouds animated

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,5 +7,12 @@ class Cloud extends MovableObject {
             "assets/img/assets/images/5_background/layers/4_clouds/1.png",
         );
         this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }
