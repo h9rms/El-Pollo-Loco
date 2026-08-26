187668ca46aca7cda8cd874ca425093135ceac37
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 22:03:48 +0200

    js code added

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
     height = 280;
     y = 155;
+    speed = 10;
     IMAGES_WALKING = [
         "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
         "assets/img/assets/images/2_character_pepe/2_walk/W-22.png",
@@ -21,12 +22,22 @@ class Character extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.x += this.speed;
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
     jump() {}
