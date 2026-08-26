d915967a8cdc9e2123f5586f3a5ea19c8878ff8a
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 20:30:26 +0200

    js cide added

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -6,6 +6,7 @@ class MovableObject {
     width = 100;
     imageCache = {};
     currentImage = 0;
+    speed = 0.15;
 
     loadImage(path) {
         this.img = new Image();
@@ -24,5 +25,9 @@ class MovableObject {
         console.log("Move right");
     }
 
-    moveLeft() {}
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
+    }
 }
