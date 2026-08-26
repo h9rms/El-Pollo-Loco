a3957e8a3852d162a63e5167ac6048941b545fdf
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 19:19:07 +0200

    bug fixed

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,14 @@ class Character extends MovableObject {
     y = 155;
     constructor(){
         super().loadImage("assets/img/assets/images/2_character_pepe/2_walk/W-21.png");
+        this.loadImages([
+            "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
+            "assets/img/assets/images/2_character_pepe/2_walk/W-22.png",
+            "assets/img/assets/images/2_character_pepe/2_walk/W-23.png",
+            "assets/img/assets/images/2_character_pepe/2_walk/W-24.png",
+            "assets/img/assets/images/2_character_pepe/2_walk/W-25.png",
+            "assets/img/assets/images/2_character_pepe/2_walk/W-26.png"
+        ]);
     }
 
     jump (){
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,17 +4,24 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
-    loadImage(path){
+    loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
     }
 
     moveRight() {
         console.log("Move right");
     }
 
-    moveLeft(){
-
-    }
+    moveLeft() {}
 }
