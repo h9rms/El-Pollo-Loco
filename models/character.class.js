15ac259cf19c84c5bd89ff92eeed8c6210a1c376
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 19:49:24 +0200

    animation addded

--- models/character.class.js
+++ models/character.class.js
@@ -9,7 +9,6 @@ class Character extends MovableObject {
         "assets/img/assets/images/2_character_pepe/2_walk/W-25.png",
         "assets/img/assets/images/2_character_pepe/2_walk/W-26.png",
     ];
-    currentImage = 0;
     constructor() {
         super().loadImage(
             "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,10 +2,26 @@ class Chicken extends MovableObject {
     y = 350;
     height = 80;
     width = 80;
+    IMAGES_WALKING = [
+        "assets/img/assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+        "assets/img/assets/images/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+        "assets/img/assets/images/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
+    ];
     constructor() {
         super().loadImage(
-            "assets/img/assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+            "assets/img/assets/images/3_enemies_chicken/chicken_normal/1_walk/1_w.png"
         );
+        this.loadImages(this.IMAGES_WALKING);
         this.x = 200 + Math.random() * 500;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 200);
     }
 }
