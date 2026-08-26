b11defae943c06877dce2a7f882763143f1d424d
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-26 19:36:11 +0200

    animation added

--- models/character.class.js
+++ models/character.class.js
@@ -1,19 +1,31 @@
 class Character extends MovableObject {
     height = 280;
     y = 155;
-    constructor(){
-        super().loadImage("assets/img/assets/images/2_character_pepe/2_walk/W-21.png");
-        this.loadImages([
+    IMAGES_WALKING = [
+        "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
+        "assets/img/assets/images/2_character_pepe/2_walk/W-22.png",
+        "assets/img/assets/images/2_character_pepe/2_walk/W-23.png",
+        "assets/img/assets/images/2_character_pepe/2_walk/W-24.png",
+        "assets/img/assets/images/2_character_pepe/2_walk/W-25.png",
+        "assets/img/assets/images/2_character_pepe/2_walk/W-26.png",
+    ];
+    currentImage = 0;
+    constructor() {
+        super().loadImage(
             "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
-            "assets/img/assets/images/2_character_pepe/2_walk/W-22.png",
-            "assets/img/assets/images/2_character_pepe/2_walk/W-23.png",
-            "assets/img/assets/images/2_character_pepe/2_walk/W-24.png",
-            "assets/img/assets/images/2_character_pepe/2_walk/W-25.png",
-            "assets/img/assets/images/2_character_pepe/2_walk/W-26.png"
-        ]);
+        );
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
     }
 
-    jump (){
-
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length;
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);
     }
+
+    jump() {}
 }
