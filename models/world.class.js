25ff327c11ae7106290b8d4e33c7d10dc2401f4b
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-27 15:45:23 +0200

    code fixed

--- models/world.class.js
+++ models/world.class.js
@@ -1,32 +1,8 @@
 class World {
     character = new Character();
-    enemies = [new Chicken(), new Chicken(), new Chicken()];
-    clouds = [new Cloud()];
-    backgroundObject = [
-        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",-720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",-720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",-720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",-720,),
-
-        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",0,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/1.png",0,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/1.png",0,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/1.png",0,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",720,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",720,),
-        
-        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720*2),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/1.png",720*2,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/1.png",720*2,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/1.png",720*2,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720*3,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",720*3,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",720*3,),
-        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",720*3,),
-    ];
-
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObject = level1.backgroundObject;
     canvas;
     ctx;
     keyboard;
