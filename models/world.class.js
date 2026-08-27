411139246b6cb57ffe7626aafe860c3b3daa0c99
Hermann Ejrich <hermannejrich@icloud.com>
2026-08-27 15:25:51 +0200

    background fixed

--- models/world.class.js
+++ models/world.class.js
@@ -3,23 +3,30 @@ class World {
     enemies = [new Chicken(), new Chicken(), new Chicken()];
     clouds = [new Cloud()];
     backgroundObject = [
-        new BackgroundObject(
-            "assets/img/assets/images/5_background/layers/air.png",
-            0,
-        ),
-        new BackgroundObject(
-            "assets/img/assets/images/5_background/layers/3_third_layer/1.png",
-            0,
-        ),
-        new BackgroundObject(
-            "assets/img/assets/images/5_background/layers/2_second_layer/1.png",
-            0,
-        ),
-        new BackgroundObject(
-            "assets/img/assets/images/5_background/layers/1_first_layer/1.png",
-            0,
-        ),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",-720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",-720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",-720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",-720,),
+
+        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",0,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/1.png",0,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/1.png",0,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/1.png",0,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",720,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",720,),
+        
+        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720*2),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/1.png",720*2,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/1.png",720*2,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/1.png",720*2,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",720*3,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",720*3,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/2_second_layer/2.png",720*3,),
+        new BackgroundObject("assets/img/assets/images/5_background/layers/1_first_layer/2.png",720*3,),
     ];
+
     canvas;
     ctx;
     keyboard;
