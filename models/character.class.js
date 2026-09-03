c86e869cd34f5adff869b6400c08eb3a0fcffca4
Hermann Ejrich <hermannejrich@icloud.com>
2026-09-03 20:13:50 +0200

    js code added

--- models/character.class.js
+++ models/character.class.js
@@ -10,32 +10,58 @@ class Character extends MovableObject {
         "assets/img/assets/images/2_character_pepe/2_walk/W-25.png",
         "assets/img/assets/images/2_character_pepe/2_walk/W-26.png",
     ];
+
+    IMAGES_JUMPING = [
+        "assets/img/assets/images/2_character_pepe/3_jump/J-31.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-32.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-33.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-34.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-35.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-36.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-37.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-38.png",
+        "assets/img/assets/images/2_character_pepe/3_jump/J-39.png"
+    ];
+
     world;
     constructor() {
         super().loadImage(
             "assets/img/assets/images/2_character_pepe/2_walk/W-21.png",
         );
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+            if (
+                this.world.keyboard.RIGHT &&
+                this.x < this.world.level.level_end_x
+            ) {
                 this.x += this.speed;
                 this.otherDirection = false;
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+            }            
+
+            if (this.world.keyboard.UP) {
+                this.speedY = 20;
             }
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
             }
         }, 50);
     }
