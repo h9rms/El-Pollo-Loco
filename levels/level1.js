2b1f71bdc3e81c61978780b5a1c2325cdcc3adf6
Hermann Ejrich <hermannejrich@icloud.com>
2026-09-03 18:49:14 +0200

    code added and bug fixed

--- levels/level1.js
+++ levels/level1.js
@@ -1,6 +1,13 @@
 const level1 = new Level(
-    [new Chicken(), new Chicken(), new Chicken()],
-    [new Cloud()],
+    [
+    new Chicken(), 
+    new Chicken(), 
+    new Chicken(),
+    new Endboss(),
+    ],
+    [
+        new Cloud()
+    ],
     [
     new BackgroundObject("assets/img/assets/images/5_background/layers/air.png",-720,),
     new BackgroundObject("assets/img/assets/images/5_background/layers/3_third_layer/2.png",-720,),
