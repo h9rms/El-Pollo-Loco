e783b36ba1065c862ba311129fe70d6337f38e01
Hermann Ejrich <hermannejrich@icloud.com>
2026-09-07 14:46:26 +0200

    js code added

---
+++ models/status-bar.class.js
@@ -0,0 +1,38 @@
+class StatusBar extends DrawableObject {
+    IMAGES = [
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png",
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png",
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png",
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png",
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png",
+        "assets/img/assets/images/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png",
+    ];
+
+    percentage = 100;
+
+    constructor() {
+        this.loadImages(this.IMAGES);
+    }
+
+    setPercentage(percentage) {
+        this.percentage = percentage;
+        let path = this.IMAGES[this.resolveImageIndex()];
+        this.img = this.imageCache[path];
+    }
+
+    resolveImageIndex() {
+        if (this.percentage == 100) {
+            return 5;
+        } else if (this.percentage > 80) {
+            return 4;
+        } else if (this.percentage > 60) {
+            return 3;
+        } else if (this.percentage > 40) {
+            return 2;
+        } else if (this.percentage > 20) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
