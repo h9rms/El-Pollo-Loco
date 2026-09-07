f8a53a85a774205676bc4721fb089e49538ff1a5
Hermann Ejrich <hermannejrich@icloud.com>
2026-09-07 14:25:53 +0200

    code added

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
@@ -24,15 +17,6 @@ class MovableObject {
 
     isAboveGround() {
         return this.y < 150;
-    }
-
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx) {
@@ -71,14 +55,6 @@ class MovableObject {
 
     isDead(){
         return this.energy == 0;
-    }
-
-    loadImages(arr) {
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
     }
 
     playAnimation(images) {
